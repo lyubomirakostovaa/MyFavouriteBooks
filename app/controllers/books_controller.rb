a29febec314f5c0e962e0b564d16336439704e03
lyubomirakostovaa <lyubomira.kostova1@gmail.com>
2021-01-17 22:58:19 +0200

    Up to exercise 7.1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,8 +17,12 @@ class BooksController < ApplicationController
     params.require(:book)
     permitted = params[:book].permit(:title,:genre,:publish_date,:isbn)
     @book = Book.create!(permitted)
+    if @book.save
     flash[:notice] = "#{@book.title} was successfully created."
     redirect_to books_path
+    else
+    render 'new'
+    end
   end
 
   def edit
@@ -26,12 +30,15 @@ class BooksController < ApplicationController
   end
    
   def update
-    @book = Book.find params[:id]
+  @book = Book.find params[:id]
     params.require(:book)
     permitted = params[:book].permit(:title,:genre,:publish_date,:isbn)
-    @book.update_attributes!(permitted)
+    if @book.update_attributes(permitted)
     flash[:notice] = "#{@book.title} was successfully updated."
     redirect_to book_path(@book)
+    else
+    render 'edit'
+    end
   end
 
   def destroy
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,19 @@
 class Book < ApplicationRecord
-end
+    def self.all_genres ; %w['Science fiction' 'Drama' 'Action and Adventure' 'Romance' 'Mystery' 'Horror'] ; end
+    validates :title, :presence => true
+    validates :publish_date, :presence => true
+    validate :published_1967_or_later 
+    validates :isbn, :unless => :grandfathered?
+    validate :genre, :inclusion  => {:in => Book.all_genres}
+   
+    def published_1967_or_later
+      errors.add(:publish_date, 'must be 1967 or later') if
+      publish_date && publish_date < Date.parse('1 Jan 1967')
+    end
+   
+    @@grandfathered_date = Date.parse('1 Jan 1967')
+   
+    def grandfathered?
+      publish_date && publish_date < @@grandfathered_date
+    end
+  end
